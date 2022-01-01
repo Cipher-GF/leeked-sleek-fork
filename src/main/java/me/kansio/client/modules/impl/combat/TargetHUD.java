56d038a94bfb8c0b7d5f5fdf9a17d4ce20a9d66f
Kansio <45654930+Kansioo@users.noreply.github.com>
2022-01-02 01:40:25 +0200

    fuck you github

--- src/main/java/me/kansio/client/modules/impl/combat/TargetHUD.java
+++ src/main/java/me/kansio/client/modules/impl/combat/TargetHUD.java
@@ -5,10 +5,19 @@ import me.kansio.client.utils.Util;
 import me.kansio.client.utils.render.RenderUtils;
 import net.minecraft.entity.EntityLivingBase;
 
+import java.awt.*;
+
 public class TargetHUD extends Util {
 
     public static void draw(RenderOverlayEvent event, EntityLivingBase target) {
-        RenderUtils.drawBorderedRoundedRect(150, 350, 150, 90, 10, 2, 2, 0x80000000
-        );
+        RenderUtils.drawBorderedRoundedRect(150, 350, 150, 90, 10, 2, 2, new Color(target.hurtTime * 6, 0, 0, 100).getRGB());
+
+        RenderUtils.drawBorderedRoundedRect(155, 390, (float) (target.getHealth() * 6.9), 15, 5, 2, 2, new Color(0, 255, 0, 255).getRGB());
+        RenderUtils.drawBorderedRoundedRect(155, 410, (float) (target.getTotalArmorValue() * 6.9), 15, 5, 2, 2, new Color(0, 255, 255, 255).getRGB());
+        //RenderUtils.drawBorderedRoundedRect(150, 350, 150, 90, 10, 2, 2, new Color(0, 255, 255, 255).getRGB());
+
+        mc.fontRendererObj.drawStringWithShadow(target.getName(), 160, 365, -1);
+        mc.fontRendererObj.drawStringWithShadow("Health: " + target.getHealth(), 160, 393, -1);
+        mc.fontRendererObj.drawStringWithShadow("Armor: " + target.getTotalArmorValue(), 16, 410, -1);
     }
 }
