8e161dd0a56d5b1915707928af0674fdb98a3d2d
DivineEnergylol <80833514+DivineEnergylol@users.noreply.github.com>
2022-01-02 13:01:47 -0500

    neeeeeeeeeega cat

--- src/main/java/me/kansio/client/modules/impl/visuals/DeathEffect.java
+++ src/main/java/me/kansio/client/modules/impl/visuals/DeathEffect.java
@@ -12,8 +12,9 @@ import net.minecraft.network.play.server.S02PacketChat;
 
 public class DeathEffect extends Module {
 
-    public ModeValue mode = new ModeValue("Effect Mode", this, "Blood", "Lightning");
+    public ModeValue mode = new ModeValue("Effect Mode", this, "Blood", "Lightning", "OOF Mod");
     public BooleanValue soundon = new BooleanValue("Play Sound", this, false);
+    public ModeValue soundon = new ModeValue("Play Sound", this, "Windows", "Yoda", );
 
     public DeathEffect() {
         super("DeathEffect", ModuleCategory.VISUALS);
@@ -32,6 +33,7 @@ public class DeathEffect extends Module {
 
     @Subscribe
     public void onChat(PacketEvent event) {
+        if (!mode.getValue().equals("OOF Mod")) return;
         if (event.getPacket() instanceof S02PacketChat) {
             S02PacketChat packet = event.getPacket();
 
@@ -39,7 +41,7 @@ public class DeathEffect extends Module {
             final String[] split = msg.split(" ");
             for (String trigger : deathMessage) {
                 if (msg.contains(trigger) && msg.contains(mc.thePlayer.getName()) && !split[0].equalsIgnoreCase(mc.thePlayer.getName())) {
-
+                    playSound("windows");
                 }
             }
         }
