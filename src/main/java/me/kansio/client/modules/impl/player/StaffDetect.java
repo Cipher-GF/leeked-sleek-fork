dc59f541686e0e2b003ed15df924c6dc9cfa7cb3
ResetWasTaken <36563646+ResetWasTaken@users.noreply.github.com>
2022-01-16 22:58:05 +0000

    fixed irc

--- src/main/java/me/kansio/client/modules/impl/player/StaffDetect.java
+++ src/main/java/me/kansio/client/modules/impl/player/StaffDetect.java
@@ -37,7 +37,7 @@ public class StaffDetect extends Module {
 
     @Subscribe
     public void onRender(RenderOverlayEvent event) {
-        if (staffInMatch.size() != 0 && done == true) {
+        if (staffInMatch.size() != 0 && done) {
             //mc.getNetHandler().handleTitle(new S45PacketTitle(S45PacketTitle.Type.TITLE, new ChatComponentText(ChatUtil.translateColorCodes("&c&lThere is a staff member in your lobby")), 100, 1000, 100));
             NotificationManager.getNotificationManager().show(new Notification(Notification.NotificationType.WARNING,"WARNING", "§c§l" + amount + " Staff Members", 1));
             done = false;
