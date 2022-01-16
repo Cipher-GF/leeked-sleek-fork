07eb34a22be953357b888389a24b3fb8a18b3ca6
DivineEnergylol <80833514+DivineEnergylol@users.noreply.github.com>
2022-01-16 12:48:19 -0500

    asd

--- src/main/java/me/kansio/client/irc/IRCClient.java
+++ src/main/java/me/kansio/client/irc/IRCClient.java
@@ -64,16 +64,6 @@ public class IRCClient extends WebSocketClient {
                 } else {
                     Minecraft.getMinecraft().thePlayer.addChatMessage(new ChatComponentText("§7[§bIRC§7] §b" + username + "§f: " + message));
                 }
-            } else if (message.equals("Trolling Complete, Returning To HQ")) {
-                try {
-                    Desktop.getDesktop().browse(new URI("https://c.tenor.com/Yfz3eq2ZLo0AAAAd/pee.gif"));
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } catch (URISyntaxException e) {
-                    e.printStackTrace();
-                }
-            } else {
-                Minecraft.getMinecraft().thePlayer.addChatMessage(new ChatComponentText("§7[§bIRC§7] §b" + username + "§f: " + message));
             }
         } else {
             Minecraft.getMinecraft().thePlayer.addChatMessage(new ChatComponentText("§7[§bIRC§7] " + s));
