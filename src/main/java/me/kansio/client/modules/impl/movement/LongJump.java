38603d126b54ac2e95b31f3290572302ba6cf943
DivineEnergylol <80833514+DivineEnergylol@users.noreply.github.com>
2022-01-23 16:20:58 -0500

    ghoslty rapery (sleek x diablo) DTB on top

--- src/main/java/me/kansio/client/modules/impl/movement/LongJump.java
+++ src/main/java/me/kansio/client/modules/impl/movement/LongJump.java
@@ -2,13 +2,18 @@ package me.kansio.client.modules.impl.movement;
 
 import com.google.common.eventbus.Subscribe;
 import me.kansio.client.event.impl.MoveEvent;
+import me.kansio.client.event.impl.RenderOverlayEvent;
 import me.kansio.client.event.impl.UpdateEvent;
 import me.kansio.client.modules.api.ModuleCategory;
 import me.kansio.client.modules.api.ModuleData;
 import me.kansio.client.modules.impl.Module;
 import me.kansio.client.property.value.ModeValue;
 import me.kansio.client.property.value.NumberValue;
+import me.kansio.client.utils.math.Stopwatch;
 import me.kansio.client.utils.player.PlayerUtil;
+import net.minecraft.network.play.client.C03PacketPlayer;
+
+import java.text.DecimalFormat;
 
 @ModuleData(
         name = "Long Jump",
@@ -21,16 +26,18 @@ public class LongJump extends Module {
     boolean launched = false;
     boolean wasLaunched = false;
     boolean jumped = false;
-    private ModeValue mode = new ModeValue("Mode", this, "Verus", "Viper", "Vanilla");
+    private ModeValue mode = new ModeValue("Mode", this, "Verus", "Viper", "Vanilla", "Test");
     //verus boost stuff
-    private NumberValue vertical = new NumberValue("Vertical Boost", this, 0.8, 0.05, 6.0, 0.1);
-    private NumberValue boost = new NumberValue("Speed", this, 1.45, 0.05, 10.0, 0.1);
+    private NumberValue<Double> vertical = new NumberValue<>("Vertical Boost", this, 0.8, 0.05, 6.0, 0.1);
+    private NumberValue<Double> boost = new NumberValue<>("Speed", this, 1.45, 0.05, 10.0, 0.1);
+    private Stopwatch damageWaiterThing = new Stopwatch();
 
     @Override
     public void onEnable() {
         launched = false;
         wasLaunched = false;
         jumped = false;
+        damageWaiterThing.resetTime();
 
 
         switch (mode.getValue()) {
@@ -80,6 +87,28 @@ public class LongJump extends Module {
                 }
                 break;
             }
+            case "Test": {
+                if (damageWaiterThing.timeElapsed(1000L)) {
+                    double x = mc.thePlayer.posX;
+                    double y = mc.thePlayer.posY;
+                    double z = mc.thePlayer.posZ;
+                    for (short i = 0; i <= ((4) / 0.0625); i++) {
+                        mc.getNetHandler().getNetworkManager().sendPacket(new C03PacketPlayer.C04PacketPlayerPosition(mc.thePlayer.posX, mc.thePlayer.posY + ((0.0625 / 2) * 1), mc.thePlayer.posZ, false));
+                        mc.getNetHandler().getNetworkManager().sendPacket(new C03PacketPlayer.C04PacketPlayerPosition(mc.thePlayer.posX, mc.thePlayer.posY + ((0.0625 / 2) * 2), mc.thePlayer.posZ, false));
+                        mc.getNetHandler().getNetworkManager().sendPacket(new C03PacketPlayer.C04PacketPlayerPosition(mc.thePlayer.posX, mc.thePlayer.posY, mc.thePlayer.posZ, (i == ((4) / 0.0625))));
+                    }
+                    damageWaiterThing.resetTime();
+                    break;
+                }
+            }
+
+        }
+    }
+
+    @Subscribe
+    public void render(RenderOverlayEvent event) {
+        if (mode.getValue().equals("Test")) {
+            mc.fontRendererObj.drawStringWithShadow(new DecimalFormat("0.#").format(((double) (damageWaiterThing.getTimeRemaining(1000)) / 1000)), (float) event.getSr().getScaledWidth_double() / 2, (float) event.getSr().getScaledHeight_double() / 2, -1);
         }
     }
 
@@ -97,6 +126,7 @@ public class LongJump extends Module {
                 }
                 break;
             }
+
         }
     }
 
---
+++ src/main/java/me/kansio/client/modules/impl/movement/speed/misc/Invaded.java
@@ -0,0 +1,9 @@
+package me.kansio.client.modules.impl.movement.speed.misc;
+
+import me.kansio.client.modules.impl.movement.speed.SpeedMode;
+
+public class Invaded extends SpeedMode {
+    public Invaded() {
+        super("Test 2");
+    }
+}
