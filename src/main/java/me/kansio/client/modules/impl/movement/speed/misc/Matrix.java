bc9ed10a8fa84f6581db21c158b97190e92a9a07
Kansio <45654930+Kansioo@users.noreply.github.com>
2022-01-25 20:06:31 +0200

    Update Matrix.java

--- src/main/java/me/kansio/client/modules/impl/movement/speed/misc/Matrix.java
+++ src/main/java/me/kansio/client/modules/impl/movement/speed/misc/Matrix.java
@@ -3,16 +3,33 @@ package me.kansio.client.modules.impl.movement.speed.misc;
 import me.kansio.client.event.impl.UpdateEvent;
 import me.kansio.client.modules.impl.movement.speed.SpeedMode;
 import me.kansio.client.modules.impl.player.Timer;
+import me.kansio.client.utils.chat.ChatUtil;
 import me.kansio.client.utils.math.BPSUtil;
 import me.kansio.client.utils.network.PacketUtil;
 import me.kansio.client.utils.player.PlayerUtil;
 import me.kansio.client.utils.player.TimerUtil;
 import net.minecraft.network.play.client.C08PacketPlayerBlockPlacement;
+import viamcp.ViaMCP;
 
 public class Matrix extends SpeedMode {
-    
+
     public Matrix() {
         super("Matrix Hop");
+    }
+
+    @Override
+    public void onEnable() {
+        if (ViaMCP.PROTOCOL_VERSION != 755) {
+            ChatUtil.log("§cYou must use 1.17 with viaversion to use this mode.");
+            getSpeed().toggle();
+        }
+
+        mc.gameSettings.keyBindJump.pressed = true;
+    }
+
+    @Override
+    public void onDisable() {
+        mc.gameSettings.keyBindJump.pressed = false;
     }
 
     @Override
