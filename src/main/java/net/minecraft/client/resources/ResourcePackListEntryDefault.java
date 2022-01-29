67decf1401236b1e084c329f28dab62fe71a3506
Kansio <45654930+Kansioo@users.noreply.github.com>
2022-01-29 18:28:35 +0200

    fix packs crashing game

--- src/main/java/net/minecraft/client/resources/ResourcePackListEntryDefault.java
+++ src/main/java/net/minecraft/client/resources/ResourcePackListEntryDefault.java
@@ -21,24 +21,11 @@ public class ResourcePackListEntryDefault extends ResourcePackListEntry
 {
     private static final Logger logger = LogManager.getLogger();
     private final IResourcePack field_148320_d;
-    private final ResourceLocation resourcePackIcon;
 
     public ResourcePackListEntryDefault(GuiScreenResourcePacks resourcePacksGUIIn)
     {
         super(resourcePacksGUIIn);
         this.field_148320_d = this.mc.getResourcePackRepository().rprDefaultResourcePack;
-        DynamicTexture dynamictexture;
-
-        try
-        {
-            dynamictexture = new DynamicTexture(this.field_148320_d.getPackImage());
-        }
-        catch (IOException var4)
-        {
-            dynamictexture = TextureUtil.missingTexture;
-        }
-
-        this.resourcePackIcon = this.mc.getTextureManager().getDynamicTextureLocation("texturepackicon", dynamictexture);
     }
 
     protected int func_183019_a()
@@ -96,7 +83,7 @@ public class ResourcePackListEntryDefault extends ResourcePackListEntry
 
     protected void func_148313_c()
     {
-        this.mc.getTextureManager().bindTexture(this.resourcePackIcon);
+
     }
 
     protected boolean func_148310_d()
