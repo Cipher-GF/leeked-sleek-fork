fe3159553623fd5e98a1e730b56e41217d87a8bb
Qoft <patiscoolig@gmail.com>
2022-02-17 23:14:30 -0500

    n

--- src/main/java/me/kansio/client/modules/impl/visuals/NoRender.java
+++ src/main/java/me/kansio/client/modules/impl/visuals/NoRender.java
@@ -8,9 +8,16 @@ import me.kansio.client.value.value.BooleanValue;
 import me.kansio.client.value.value.NumberValue;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.item.EntityItem;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.network.Packet;
 import net.minecraft.network.Packet;
 import net.minecraft.network.play.server.*;
+import net.minecraft.util.BlockPos;
+import net.minecraft.world.WorldSettings;
+
+import java.util.Random;
 
 @ModuleData(
         name = "NoRender",
@@ -18,8 +25,8 @@ import net.minecraft.network.play.server.*;
         description = "Items Dropped Have Physics"
 )
 public class NoRender extends Module {
-    private final BooleanValue  noWeather  = new BooleanValue("No Weather", this, false);
-    private final BooleanValue  items  = new BooleanValue("Items", this, false);
+    private final BooleanValue noWeather  = new BooleanValue("No Weather", this, false);
+    private final BooleanValue items  = new BooleanValue("Items", this, false);
 
 
     @Subscribe
@@ -31,4 +38,6 @@ public class NoRender extends Module {
             NoRender.mc.theWorld.setRainStrength(0.0f);
         }
     }
+
+
 }
