75a90e8067dc9c66977709938cb9d508531e7544
Kansio <45654930+Kansioo@users.noreply.github.com>
2022-03-01 17:56:14 +0200

    dfg

---
+++ src/main/java/me/kansio/client/commands/impl/HideModuleCommand.kt
@@ -0,0 +1,32 @@
+package me.kansio.client.commands.impl
+
+import me.kansio.client.Client
+import me.kansio.client.commands.Command
+import me.kansio.client.commands.CommandData
+import me.kansio.client.modules.impl.Module
+import me.kansio.client.utils.chat.ChatUtil
+import java.util.function.Consumer
+
+/**
+ * @author Kansio
+ */
+@CommandData(
+    name = "hidemodule",
+    description = "Hides a module"
+)
+class HideModuleCommand : Command() {
+    override fun run(args: Array<String>) {
+        if (args.size != 1) {
+            ChatUtil.log("Please specify a module to hide. You can also hide all modules by typing 'all'")
+            return
+        }
+        if (args[0].equals("all", ignoreCase = true)) {
+            Client.getInstance().moduleManager.modules.forEach(Consumer { module: Module -> module.isHidden = true })
+            ChatUtil.log("You've hid all the modules.")
+            return
+        }
+        val name = args[0]
+        Client.getInstance().moduleManager.getModuleByNameIgnoreSpace(name).isHidden = true
+        ChatUtil.log("You've hid the module '$name'.")
+    }
+}
