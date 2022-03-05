402ac61bdb7d5240f835bb23e4ef94f620b5f4a0
Kansio <45654930+Kansioo@users.noreply.github.com>
2022-03-05 20:06:46 +0200

    sf

--- src/main/java/today/sleek/client/modules/impl/movement/speed/watchdog/Bhop.java
+++ src/main/java/today/sleek/client/modules/impl/movement/speed/watchdog/Bhop.java
@@ -5,6 +5,7 @@ import net.minecraft.util.MathHelper;
 import today.sleek.base.event.impl.MoveEvent;
 import today.sleek.base.event.impl.UpdateEvent;
 import today.sleek.client.modules.impl.movement.speed.SpeedMode;
+import today.sleek.client.utils.chat.ChatUtil;
 import today.sleek.client.utils.player.PlayerUtil;
 
 
@@ -22,7 +23,20 @@ public class Bhop extends SpeedMode {
     public void onUpdate(UpdateEvent event) {
         if (event.isPre()) {
             if (mc.thePlayer.onGround) {
-                mc.thePlayer.jump();
+                mc.thePlayer.motionY = 0.311;
+
+                float f = mc.thePlayer.rotationYaw * 0.017453292F;
+                if (!mc.thePlayer.isPotionActive(Potion.moveSpeed)) {
+                    mc.thePlayer.motionX -= MathHelper.sin(f) * 0.09F;
+                    mc.thePlayer.motionZ += MathHelper.cos(f) * 0.09F;
+                } else {
+                    mc.thePlayer.motionX -= MathHelper.sin(f) * 0.23F;
+                    mc.thePlayer.motionZ += MathHelper.cos(f) * 0.23F;
+                }
+            }
+
+            if (mc.thePlayer.fallDistance > 0.29) {
+                //mc.thePlayer.motionY = -0.253;
             }
         }
     }
