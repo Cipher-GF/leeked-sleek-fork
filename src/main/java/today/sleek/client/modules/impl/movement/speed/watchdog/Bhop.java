7f296ae4c8361f879cfe5d1f37b3fb23cf9ca012
Kansio <45654930+Kansioo@users.noreply.github.com>
2022-03-05 18:15:46 +0200

    sdf

--- src/main/java/today/sleek/client/modules/impl/movement/speed/watchdog/Bhop.java
+++ src/main/java/today/sleek/client/modules/impl/movement/speed/watchdog/Bhop.java
@@ -1,6 +1,7 @@
 package today.sleek.client.modules.impl.movement.speed.watchdog;
 
 import net.minecraft.potion.Potion;
+import net.minecraft.util.MathHelper;
 import today.sleek.base.event.impl.MoveEvent;
 import today.sleek.base.event.impl.UpdateEvent;
 import today.sleek.client.modules.impl.movement.speed.SpeedMode;
@@ -15,33 +16,18 @@ public class Bhop extends SpeedMode {
 
     @Override
     public void onEnable() {
-        getSpeed().getHDist().set(0.37245 + (mc.thePlayer.isPotionActive(Potion.moveSpeed) ? 0.5 : 0));
     }
 
     @Override
     public void onUpdate(UpdateEvent event) {
-        getSpeed().getHDist().set(0.37245 + (mc.thePlayer.isPotionActive(Potion.moveSpeed) ? 0.5 : 0));
-        event.setRotationYaw((float) PlayerUtil.getDirection());
+        if (event.isPre()) {
+            if (mc.thePlayer.onGround) {
+                mc.thePlayer.jump();
+            }
+        }
     }
 
     @Override
     public void onMove(MoveEvent event) {
-        if (mc.thePlayer.isCollidedHorizontally) {
-            mc.thePlayer.motionX = 0;
-            mc.thePlayer.motionZ = 0;
-            return;
-        }
-        if (mc.thePlayer.isMovingOnGround()) {
-            event.setMotionY(mc.thePlayer.motionY = 0.42f);
-        }
-        double speed = getSpeed().getHDist().get();
-        PlayerUtil.setMotion(speed);
-        if (!mc.thePlayer.onGround) {
-            // for friction
-            event.setMotionX(mc.thePlayer.motionX *= 0.80);
-            event.setMotionZ(mc.thePlayer.motionZ *= 0.80);
-        }
     }
-
-
 }
