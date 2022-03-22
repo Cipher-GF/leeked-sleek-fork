4fae1c7958c410776bcd67b9c8df2632faf28c99
DivineEnergylol <beandog420@hotmail.com>
2022-03-22 08:11:07 -0400

    git fetch

--- src/main/java/today/sleek/client/modules/impl/movement/speed/watchdog/Hop.kt
+++ src/main/java/today/sleek/client/modules/impl/movement/speed/watchdog/Hop.kt
@@ -4,12 +4,15 @@ import today.sleek.base.event.impl.MoveEvent
 import today.sleek.client.modules.impl.movement.speed.SpeedMode
 import today.sleek.client.utils.player.PlayerUtil
 
-class Hop: SpeedMode("Watchdog (New)") {
+class Hop : SpeedMode("Watchdog (New)") {
 
     override fun onMove(event: MoveEvent) {
         if (mc.thePlayer.isMovingOnGround) {
             mc.timer.timerSpeed = 1.0f
-            event.motionY = 0.4025; also { mc.thePlayer.motionY = 0.4025 }
+            event.motionY = 0.35.also { mc.thePlayer.motionY = it }
+        }
+        if (mc.thePlayer.fallDistance > 0) {
+            event.motionY = (-0.42).also { mc.thePlayer.motionY = it }
         }
         if (!mc.thePlayer.onGround) {
             mc.timer.timerSpeed = speed.timer.value
@@ -17,7 +20,7 @@ class Hop: SpeedMode("Watchdog (New)") {
             mc.thePlayer.motionX *= 0.9
             mc.thePlayer.motionZ *= 0.9
         }
-        val speed = if (mc.thePlayer.hurtTime > 0 ) PlayerUtil.getBaseSpeed() * 1.4 else PlayerUtil.getBaseSpeed()
+        val speed = if (mc.thePlayer.hurtTime > 0) PlayerUtil.getBaseSpeed() * 1.4 else PlayerUtil.getBaseSpeed() * 0.875
         PlayerUtil.setMotion(event, speed.toDouble())
     }
 
